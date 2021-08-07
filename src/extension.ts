03bd09d55f7cd60ed57e05a1445bcaaa5416e3e5
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-07 22:53:33 +0530

    webview setup

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vodoo" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vodoo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vodoo!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vodoo.helloWorld", () => {
+			HelloWorldPanel.createOrShow(context.extensionUri);
+		})
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vodoo.askQuestion", async () => {
+			const answer = await vscode.window.showInformationMessage(
+				"how was your day?",
+				"good",
+				"bad",
+				"meh"
+			);
+			if (answer === "bad") {
+				console.log('sorry to hear that')
+			} else {
+				console.log(answer)
+			}
+		})
+	);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
