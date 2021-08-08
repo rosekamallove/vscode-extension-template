f8c7686081da97846d94be1653eec8bc6feea40a
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-09 00:10:22 +0530

    github oAuth

--- vodoo-api/src/index.ts
+++ vodoo-api/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import admin from "firebase-admin";
+import passport from "passport";
+import { Strategy as GitHubStrategy } from "passport-github";
+require("dotenv-safe");
+require("dotenv").config();
 
 const serviceAccount = require("../ServiceAccountKey.json");
 admin.initializeApp({
@@ -7,22 +11,59 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+let User;
+
+const todos: Array<{ text: string; completed: boolean }> = [
+  { text: "five", completed: false },
+  { text: "six", completed: false },
+  { text: "seven", completed: false },
+  { text: "eight", completed: false },
+];
 
 (async () => {
   const app = express();
-
-  const todos: Array<{ text: string; completed: boolean }> = [
-    { text: "one", completed: false },
-    { text: "two", completed: true },
-    { text: "three", completed: false },
-    { text: "four", completed: false },
-  ];
-
-  db.collection("users").doc("rosekamallove").set({
-    todos,
+  passport.serializeUser((user: any, done) => {
+    done(null, user.accesToken);
   });
 
-  app.get("/", (req, res) => {
+  app.use(passport.initialize());
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3001/auth/github/callback",
+      },
+      (_, __, profile, cb) => {
+        console.log(profile);
+        cb(null, {
+          accesToken: "ksdjfaskdfjk",
+          refresToken: "kasdjfalskdfjale",
+        });
+        User = db.collection("users").doc(profile.id);
+        User.set({
+          todos,
+        });
+      }
+    )
+  );
+
+  // db.collection("users").doc("rosekamallove").set({
+  //   todos,
+  // });
+
+  app.get("/auth/github", passport.authenticate("github", { session: false }));
+
+  app.get(
+    "/auth/github/callback",
+    passport.authenticate("github", { session: false }),
+    (_req, res) => {
+      res.send("logged in succesfully");
+    }
+  );
+
+  app.get("/", (_req, res) => {
     res.send("hello");
   });
 
---
+++ vodoo-api/src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+  }
+}
