72f0af51996775b83ac42e191631bb45fe05817c
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-08 16:13:47 +0530

    send commands from svelte

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -30,6 +30,22 @@
       </li>
     {/each}
   </ul>
+  <button
+    on:click={() => {
+      tsvscode.postMessage({
+        type: "onInfo",
+        value: "hello from svelte",
+      });
+    }}>Click Me</button
+  >
+  <button
+    on:click={() => {
+      tsvscode.postMessage({
+        type: "onError",
+        value: "hello from svelte",
+      });
+    }}>Show Error</button
+  >
 </main>
 
 <style>
---
+++ webviews/globals.d.ts
@@ -0,0 +1,8 @@
+import * as _vscode from "vscode";
+const tvscode = _vscode;
+
+declare global {
+  const tsvscode: {
+    postMessage: ({ type: string, value: any }) => void;
+  };
+}
