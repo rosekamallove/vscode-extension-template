b173920dc275c4d9588084631c665970dac1bbaa
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-08 22:03:55 +0530

    firestore setup

--- vodoo-api/src/index.ts
+++ vodoo-api/src/index.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import admin from "firebase-admin";
+
+const serviceAccount = require("../ServiceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 (async () => {
   const app = express();
+
+  const todos: Array<{ text: string; completed: boolean }> = [
+    { text: "one", completed: false },
+    { text: "two", completed: true },
+    { text: "three", completed: false },
+    { text: "four", completed: false },
+  ];
+
+  db.collection("users").doc("rosekamallove").set({
+    todos,
+  });
 
   app.get("/", (req, res) => {
     res.send("hello");
