50f74af1bcb2aafbf7cebfafeb475a87612449d8
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-08 15:54:19 +0530

    todo styles

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -8,11 +8,13 @@
   <h1>Vodoo</h1>
   <form
     on:submit|preventDefault={() => {
-      todos = [{ text, completed: false }, ...todos];
+      if (text !== "") {
+        todos = [{ text, completed: false }, ...todos];
+      }
       text = "";
     }}
   >
-    <input bind:value={text} />
+    <input bind:value={text} type="text" />
   </form>
 
   <!-- Rendering the todos -->
@@ -34,7 +36,31 @@
   .complete {
     text-decoration: line-through;
   }
-  input {
-    background-color: gray;
+  input[type="text"] {
+    background-color: #282c34;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 6px 12px -2px,
+      rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  }
+  ul {
+    list-style: none;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    background-color: #282c34;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 6px 12px -2px,
+      rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    width: 100%;
+    padding: 5px;
+    margin: 10px;
+    margin-right: 20px;
+    border-radius: 5px;
+    transition: 0.5s;
+  }
+  li:hover {
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 50px 100px -20px,
+      rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
   }
 </style>
